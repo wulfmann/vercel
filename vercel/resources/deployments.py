b75a2c7b4b8fc0c71f2128b60fb3e65ed14fce4d
Joe Snell <joepsnell@gmail.com>
2020-07-26 06:45:15 -0500

    add public

--- vercel/resources/deployments.py
+++ vercel/resources/deployments.py
@@ -1,7 +1,7 @@
 from vercel.resources.base import Resource
 
 class Deployment(Resource):
-  def __init__(self, aliases, alias_assigned, alias_error, created_at, created_in, deployment_hostname, forced, id, meta, plan, private, ready_state, requested_at, target, team_id, type, url, user_id, regions, functions, routes, env, build, version, name):
+  def __init__(self, aliases, alias_assigned, alias_error, created_at, created_in, deployment_hostname, forced, id, meta, plan, private, ready_state, requested_at, target, team_id, type, url, user_id, regions, functions, routes, env, build, version, name, public):
     self.aliases = aliases
     self.alias_assigned = alias_assigned
     self.alias_error = alias_error
@@ -28,6 +28,7 @@ class Deployment(Resource):
     self.build = build
     self.version = version
     self.name = name
+    self.public = public
 
   @classmethod
   def from_data(cls, data):
@@ -58,7 +59,8 @@ class Deployment(Resource):
       env=data.get('env', []),
       build=data.get('build', { 'env': {} }),
       version=data.get('version'),
-      name=data.get('name')
+      name=data.get('name'),
+      public=data.get('public')
     )
     
   @classmethod
