d804ecaba278e3eb2ccb3c7c7995bbfa8940afd0
Joe Snell <joepsnell@gmail.com>
2020-07-26 11:12:18 -0500

    add create domain

--- tests/resources/domain_test.py
+++ tests/resources/domain_test.py
@@ -21,6 +21,64 @@ class TestDomain(TestCase):
         assert isinstance(record, vercel.DnsRecord)
         assert record.domain_name == 'test.com'
         assert record.id == 'fake-record-id'
+        
+    @patch('requests.request')
+    def test_create_v4(self, mock_request):
+        mock_v4_create = Path('tests/fixtures/responses/domains/v4/create.json')
+        mock_request.return_value = MockResponse(response=json.loads(mock_v4_create.open().read()))
+
+        domain = vercel.Domin.create('example.com')
+
+        assert isinstance(domain, vercel.Domain)
+        
+        assert domain.name == 'example.com'
+        assert domain.id == 'domain-id'
+        assert domain.service_type == 'external'
+        assert domain.ns_verified_at == None
+        assert domain.txt_verified_at == None
+        assert domain.cdn_enabled == False
+        assert domain.created_at == 1544658552174
+        assert domain.bought_at == None
+        assert domain.transferred_at == None
+        assert domain.verification_record == 'verification-id'
+        assert domain.verified == True
+        assert domain.nameservers == [
+          "ns1.nameserver.net",
+          "ns2.nameserver.net"
+        ]
+
+        assert domain.intended_nameservers == [
+          "a.zeit-world.net",
+          "b.zeit-world.co.uk",
+          "e.zeit-world.org",
+          "f.zeit-world.com"
+        ]
+
+        creator = domain.creator
+        assert isinstance(creator, vercel.Creator)
+        
+        assert creator.id == 'creator-id'
+        assert creator.username == 'zeit_user'
+        assert creator.email == 'demo@example.com'
+        
+        
+        assert [
+            call(
+                method='POST',
+                url='https://api.vercel.com/v4/domains/',
+                headers={
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer fake-api-key'
+                },
+                params={
+                    'teamId': 'fake-team-id'
+                },
+                json={
+                    'name': 'example.com'
+                }
+            )
+        ] == mock_request.mock_calls
+
 
     @patch('requests.request')
     def test_create_dns_record(self, mock_request):
--- vercel/resources/domains.py
+++ vercel/resources/domains.py
@@ -1,19 +1,79 @@
 from vercel.resources.base import Resource
 from vercel.resources.dns import DnsRecord
 
+class Creator:
+  def __init__(self, id, username, email):
+    self.id = id
+    self.username = username
+    self.email = email
+    
+  @classmethod
+  def from_data(cls, data):
+    return cls(
+      id=data['id'],
+      username=data['username'],
+      email=data['email']
+    )
+
 class Domain(Resource):
-    def __init__(self, name, api_key=None, team_id=None):
+    def __init__(self, name, id, service_type, ns_verified_at, txt_verified_at, cdn_enabled, created_at, bought_at, transferred_at, verification_record, verified, nameservers, intended_nameservers, creator):
         self.name = name
-        self.owner = {
-            'api_key': api_key,
-            'team_id': team_id
-        }
+        self.id = id
+        self.service_type = service_type
+        self.ns_verified_at = ns_verified_at
+        self.txt_verified_at = txt_verified_at
+        self.cdn_enabled = cdn_enabled
+        self.created_at = created_at
+        self.bought_at = bought_at
+        self.transferred_at = transferred_at
+        self.verification_record = verification_record
+        self.verified = verified
+        self.nameservers = nameservers
+        self.intended_nameservers = intended_nameservers
+        self.creator = creator
+        
+    @classmethod
+    def from_data(cls, data):
+      creator = data.get('creator')
+      
+      if creator is not None:
+        creator = Creator.from_data(creator)
+
+      return cls(
+        name=data['name'],
+        id=data.get('name'),
+        service_type=data.get('serviceType'),
+        ns_verified_at=data.get('nsVerifiedAt'),
+        txt_verified_at=data.get('txtVerifiedAt'),
+        cdn_enabled=data.get('cdnEnabled'),
+        created_at=data.get('createdAt'),
+        bought_at=data.get('boughtAt'),
+        transferred_at=data.get('transferredAt'),
+        verification_record=data.get('verificationRecord'),
+        verified=data.get('verified'),
+        nameservers=data.get('nameservers', []),
+        intended_nameservers=data.get('intendedNameservers', []),
+        creator=creator
+      )
 
     @classmethod
     def get(cls, name):
-        return cls(
-            name=name
-        )
+      return cls.from_data({
+        'name': name
+      })
+      
+    @classmethod
+    def create(cls, name, api_version='v4'):
+      res = cls.make_request(
+        method='POST',
+        resource=f'/domains',
+        data={
+          'name': name
+        },
+        api_version=api_version
+      )
+      
+      return cls.from_data(res)
 
     def create_dns_record(self, **params):
         return DnsRecord.create(
