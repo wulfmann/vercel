1897016af5093c0712178d6c3d45ea305d3e7c89
Joe Snell <joepsnell@gmail.com>
2020-07-26 12:20:28 -0500

    fix typo

--- vercel/resources/__init__.py
+++ vercel/resources/__init__.py
@@ -2,4 +2,4 @@ from vercel.resources.dns import DnsRecord
 from vercel.resources.domains import Domain, Creator
 from vercel.resources.projects import Project, EnvironmentVariable, Alias
 from vercel.resources.deployments import Deployment
-from vercel.reaources.teams import Team
+from vercel.resources.teams import Team
